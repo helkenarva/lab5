69fa27dc0274b0fb88ca1e9c9485f113088dab07
helkenarva <helkenarva@gmail.com>
2016-05-19 11:04:18 +0600

    adapt code for tests

--- Makefile
+++ Makefile
@@ -1,13 +1,21 @@
-.PHONY: clean
+.PHONY: clean prepare
 
-bin/qwrt: obj/lab2.o obj/qwrt.o
+bin/qwrt: prepare obj/lab2.o obj/qwrt.o
 	gcc -o bin/qwrt obj/lab2.o obj/qwrt.o -lm
 
-obj/lab2.o:
+obj/lab2.o: prepare
 	gcc -c src/lab2.c -o obj/lab2.o
 
-obj/qwrt.o:
+obj/qwrt.o: prepare
 	gcc -c src/qwrt.c -o obj/qwrt.o
 
 clean:
 	rm -rf bin/* obj/*
+
+prepare: bin obj
+
+bin:
+	mkdir bin
+
+obj:
+	mkdir obj
--- src/lab2.c
+++ src/lab2.c
@@ -4,9 +4,24 @@
 
 int main()
 {
-	double a, b, c;
+	int roots;
+	double a, b, c, d, x1, x2;
 	printf("Enter a, b, c: \n");
 	scanf("%lf %lf %lf", &a, &b, &c);
-	qwrt(a, b, c);
+	roots = qwrt(a, b, c, &d, &x1, &x2);
+	if (roots == -1)
+	{
+		printf("Coefficient A should be more than zero.\n");
+		return 0;
+	}
+	
+	printf("\nDiscriminant: %lf\n", d);
+	
+	if (roots == 0)
+		printf("No roots\n");
+	else if (roots == 1)
+		printf("Root: %lf\n", x1);
+	else
+		printf("\nRoots: %lf %lf\n", x1, x2);
 	return 0;
 }
